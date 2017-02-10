f26cce1b74b56bcbb0bd34001a591040b215e053
Pavel Vovkotrub <markyarab97@gmail.com>
2017-02-10 05:03:41 +0200

    replce part1 to lesson 16, where is laying part-2(calculator JQuery)

---
+++ lesson16/part-2 calculator/app/js/common.js
@@ -0,0 +1,190 @@
+var btns = document.getElementById('keypad');
+var inputs = [];
+var hold = [];
+
+function getEventTarget(e) {
+    e = e || window.event;
+    return e.target || e.srcElement;
+}
+
+btns.onclick = function(event) {
+
+    var target = getEventTarget(event);
+
+    var screen = document.querySelector('.screen');
+    var screenVal = screen.innerHTML;
+    var btnVal = target.innerHTML;
+    var d = document.getElementsByClassName("active");
+
+
+    if (screenVal === '0' && btnVal != '.') {
+
+        screen.innerHTML = '';
+    }
+
+
+    var calculate = screenVal;
+
+
+    if (btnVal == 'C') {
+        screen.innerHTML = '0';
+        inputs = [];
+    }
+
+    else if (btnVal == '+/-') {
+
+        if (hold[0] === undefined) {
+            screen.innerHTML = '-' + calculate;
+            hold.push('-' + calculate);
+        } else {
+            calculate = calculate.replace(/-/g, '');
+            screen.innerHTML = calculate;
+            hold = [];
+        }
+
+    }
+
+    else if (btnVal == 'x') {
+        if (calculate !== '0') {
+            inputs.push(calculate);
+            if (calculate === '' && inputs[3] === undefined) {
+                inputs[1] = '*';
+            } else {
+
+                inputs.push('*');
+            }
+            screen.innerHTML = '';
+        } else {
+            inputs[1] = '*';
+        }
+
+        while (d[0]) {
+            d[0].className = 'symbol';
+        }
+
+        target.className = 'symbol active';
+
+    }
+
+    else if (btnVal == '/') {
+
+        if (calculate != '0') {
+            inputs.push(calculate);
+            if (calculate === '' && inputs[3] === undefined) {
+                inputs[1] = '/';
+            } else {
+
+                inputs.push('/');
+            }
+            screen.innerHTML = '';
+        } else {
+            inputs[1] = '/';
+        }
+
+        while (d[0]) {
+            d[0].className = 'symbol';
+        }
+
+        target.className = 'symbol active';
+
+    }
+
+    else if (btnVal == '-') {
+
+        if (calculate == '0') {
+
+            screen.innerHTML = '-';
+
+        } else {
+            inputs.push(calculate);
+            if (calculate === '' && inputs[3] === undefined) {
+                inputs[1] = '-';
+            } else {
+
+                inputs.push('-');
+            }
+            screen.innerHTML = '';
+
+        }
+
+        while (d[0]) {
+            d[0].className = 'symbol';
+        }
+
+        target.className = 'symbol active';
+    }
+
+    else if (btnVal == '+') {
+
+        if (calculate != '0') {
+
+            inputs.push(calculate);
+            if (calculate === '' && inputs[3] === undefined) {
+                inputs[1] = '+';
+            } else {
+
+                inputs.push('+');
+            }
+
+            screen.innerHTML = '';
+
+        } else {
+            inputs[1] = '+';
+        }
+
+        while (d[0]) {
+            d[0].className = 'symbol';
+        }
+
+        target.className = 'symbol active';
+
+    }
+
+    else if (btnVal == '=') {
+
+        while (d[0]) {
+            d[0].className = 'symbol'
+        }
+
+
+        inputs.push(calculate);
+        calculate = inputs.toString();
+
+
+        var lastChar = calculate[calculate.length - 1];
+
+        calculate = calculate.replace(/x/g, '*').replace(/,/g, '');
+
+        if (lastChar == '.') {
+            calculate = calculate.replace(/./, '');
+        }
+        if (lastChar == '%') {
+            var getNum = inputs[0].toString();
+            var getSy = inputs[1].toString();
+            var getPar = inputs[2].toString();
+            getPar = getPar.replace(/%/g, '');
+
+            var calculatePar = getPar / "100" * getNum;
+            calculate = getNum + getSy + calculatePar;
+
+        }
+        inputs = [];
+
+        if (calculate) {
+            var answer = eval(calculate);
+
+            if (answer.toFixed(2) === answer + ".00") {
+                screen.innerHTML = answer;
+            } else {
+                screen.innerHTML = answer.toFixed(2);
+            }
+
+        }
+    }
+    else {
+        screen.innerHTML += btnVal;
+
+    }
+
+    e.preventDefault();
+};
